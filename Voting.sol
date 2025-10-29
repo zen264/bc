91b58ad48748661d7d57ad2beb3f7a473bc45fd8
zen <53945157+zen264@users.noreply.github.com>
2025-10-30 00:12:06 +0530

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -15,7 +15,25 @@ address public chairperson;
 mapping(address => Voter) public voters
 Proposal[] public proposals;
 
-
+constructor(bytes32[] memory proposalNames) {
+chairperson = msg.sender;
+voters[chairperson].weight = 1;
+for (uint i = 0; i < proposalNames.length; i++) {
+proposals.push(Proposal({
+name: proposalNames[i],
+voteCount: 0
+}));
+}
+}
+function giveRightToVote(address voter) external {
+require(msg.sender == chairperson, "Only chairperson can give right to vote.");
+require(
+!voters[voter].voted,
+"The voter already voted."
+);
+require(voters[voter].weight == 0);
+voters[voter].weight = 1;
+}
 
 
 function delegate(address to) external {
